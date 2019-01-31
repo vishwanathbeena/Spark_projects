623d7bbbe759a9abd22d1c0158a9a92eb92c1d14
vbeena.jupudi <vbeena.jupudi@SH302184S1L.SLS.ads.valuelabs.net>
2019-01-31 20:37:40 +0530

    Committing maven configuration properties in travis.yml

---
+++ ci/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export env=$1
+export version=$2
+JAR_NAME=/home/travis/build/vishwanathbeena/travis_cics_practice/target/travis_cics_practice-${version}.jar
+CLASS_NAME="com.cicd.travis.practice.MyFirstTravisProgram"
+
+if [ ${env} = "dev" ] || [ ${env} = "staging" ] || [ ${env} = "prod" ]; then 
+    java -cp ${JAR_NAME} ${CLASS_NAME}
+else 
+    echo "Invalid Environment"
+	exit 1
+fi
